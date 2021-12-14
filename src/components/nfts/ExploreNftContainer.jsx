ab392d67ea14e5fb59f7250cc438365e4e69c745
happycamper1129 <happycamper.sw@gmail.com>
2021-12-14 10:58:27 +0300

    fix rounded corners

--- src/components/nfts/ExploreNftContainer.jsx
+++ src/components/nfts/ExploreNftContainer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const ExploreNftContainer = ({children}) => {
     return (
-        <div className="flex flex-col justify-between bg-black h-full duration-500 ease-out transform hover:scale-105"
-        >
+        <div className="flex flex-col rounded-3xl
+        justify-between bg-black h-full duration-500 ease-out transform hover:scale-105">
             {children}
         </div>
     );

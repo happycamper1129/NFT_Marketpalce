5392871777aa25d83e3f7cf2bf53b297895efec9
happycamper1129 <happycamper.sw@gmail.com>
2021-12-18 18:25:46 +0300

    Fix ring width + add empty footer

--- src/components/nfts/ExploreNftContainer.jsx
+++ src/components/nfts/ExploreNftContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExploreNftContainer = ({children}) => {
     return (
-        <div className="flex flex-col rounded-3xl ring-1 ring-light_blue
+        <div className="flex flex-col rounded-3xl ring-2 ring-light_blue
         justify-between bg-white h-full duration-500 ease-out transform hover:scale-105">
             {children}
         </div>

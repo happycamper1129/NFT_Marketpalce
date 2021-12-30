acade568c2e9a4970f497cf6a6771adb617f9a3e
happycamper1129 <happycamper.sw@gmail.com>
2021-12-30 23:11:22 +0300

    change profile tabs

--- src/redux/reducers/profile.js
+++ src/redux/reducers/profile.js
@@ -2,7 +2,6 @@ const initialState = {
     tabs: [
         {name: 'My NFT', path: '/profile-nft/all'},
         {name: 'My Listed NFT', path: '/profile-nft/listed'},
-        {name: 'My Minted NFT', path: '/profile-nft/minted'},
         {name: 'My History', path: '/profile-nft/history'},
     ]
 }

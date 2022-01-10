286e063b9844fca7dad42da4422f9b18dcf5114c
happycamper1129 <happycamper.sw@gmail.com>
2022-01-10 23:26:34 +0300

    Update view-code.ts

--- src/business-logic/near2/near/contract/view-code.ts
+++ src/business-logic/near2/near/contract/view-code.ts
@@ -1,17 +1,18 @@
 import {wallet} from "../setup/near";
 import {parseContract} from "near-contract-parser";
 import {QueryResponseKind} from "near-api-js/lib/providers/provider";
+import {JsonRpcProvider} from "near-api-js/lib/providers";
 
 interface ViewCode extends QueryResponseKind {
     code_base64: string
 }
 
 // Reverses WASM contract and extract methods
-export const viewMethods = async (contractId: string) => {
-    const near = wallet._near
-    return near.connection.provider.query<ViewCode>({
-        account_id: contractId,
-        finality: 'final',
-        request_type: 'view_code'
-    }).then(response => parseContract(response.code_base64))
-}
+export const viewMethods = (contractId: string) =>
+    new JsonRpcProvider('https://rpc.mainnet.near.org/')
+        .query<ViewCode>({
+            account_id: contractId,
+            finality: 'final',
+            request_type: 'view_code'
+        })
+        .then(response => parseContract(response.code_base64))

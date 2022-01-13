657e32f1a513b85c791e8be4ba3d02dd31061b02
happycamper1129 <happycamper.sw@gmail.com>
2022-01-13 20:13:12 +0300

    Update NearSVG.jsx

--- src/components/ui/icons/near/NearSVG.jsx
+++ src/components/ui/icons/near/NearSVG.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from '../../../../resources/near-protocol-near-logo.svg'
 
-const NearSVG = () => {
-    return <img width={16} height={16} src={logo} alt="near"/>
+const NearSVG = ({width=16, height=16}) => {
+    return <img width={height} height={width} src={logo} alt="near"/>
 };
 
 export default NearSVG;

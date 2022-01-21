8e73858baa5b323083fd13c5f612820f99fe60ff
happycamper1129 <happycamper.sw@gmail.com>
2022-01-21 14:04:28 +0300

    upd mint

--- src/business-logic/near/api/nfts/mint.js
+++ src/business-logic/near/api/nfts/mint.js
@@ -4,10 +4,8 @@ import {functionCall} from "../rpc";
 
 export function mintToCommonCollection(tokenMetadata, payout) {
     const commonContactId = 'mjol.near';
-    const tokenId = 'token-' + new Date().getTime();
 
     const args = {
-        token_id: tokenId,
         token_owner_id: getAccountId(),
         token_metadata: tokenMetadata,
     };

2db02a40641c86eee0f4616b3b97422d45e3a2da
happycamper1129 <happycamper.sw@gmail.com>
2022-02-21 09:42:58 +0300

    remove extra imports

--- src/components/Preview/Card/Status/connect-wallet/NoPriceConnectWallet.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ConnectWalletButton from "./ConnectWalletButton";
-
-const NoPriceConnectWallet = ({onClick}) => {
-    return <ConnectWalletButton onClick={onClick}/>;
-};
-
-export default NoPriceConnectWallet;

82b113008f16d80fb97c626e45249ad246b64d1a
happycamper1129 <happycamper.sw@gmail.com>
2022-03-09 21:48:31 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import AppRouter from "./AppRouter";
 import Navbar from "./pages/navbar/Navbar";
-import MarketStatsChart from "./components/Stats/Ranking/MarketStatsChart";
 
 
 export default function App() {
@@ -9,17 +8,7 @@ export default function App() {
         <>
             <Navbar/>
             <div className="pt-[69px] min-h-[calc(100vh-69px)]">
-                <MarketStatsChart data={[
-                    {volume: 20, dateString: '14 FEB 2022'},
-                    {volume: 13, dateString: '15 FEB 2022'},
-                    {volume: 29, dateString: '16 FEB 2022'},
-                    {volume: 11, dateString: '17 FEB 2022'},
-                    {volume: 14, dateString: '18 FEB 2022'},
-                    {volume: 17, dateString: '19 FEB 2022'},
-                    {volume: 20, dateString: '20 FEB 2022'},
-                    {volume: 9, dateString: '21 FEB 2022'},
-                ]}/>
-                {/*<AppRouter/>*/}
+                <AppRouter/>
             </div>
         </>
     )

9d229d97d6b45100968fcd29dfb12d17f22db418
happycamper1129 <happycamper.sw@gmail.com>
2022-03-11 14:05:28 +0300

    Update FooterPage.tsx

--- src/pages/footer/FooterPage.tsx
+++ src/pages/footer/FooterPage.tsx
@@ -5,7 +5,11 @@ const FooterPage = () => {
         <footer className="text-center bg-mjol-blue-card-property text-gray-700 mt-20">
             <div className="">
                 <div className="py-5 flex justify-center">
-                    <a href="https://twitter.com/MjolNear_" className="mr-9 text-gray-800" target="_blank" rel="noreferrer">
+                    <a href="https://twitter.com/MjolNear_"
+                       className="mr-9 text-gray-800 hover:text-blue-500"
+                       target="_blank"
+                       rel="noreferrer"
+                    >
                         <svg
                             aria-hidden="true"
                             focusable="false"
@@ -22,7 +26,11 @@ const FooterPage = () => {
                             />
                         </svg>
                     </a>
-                    <a href="https://github.com/MjolNear" className="text-gray-800" target="_blank" rel="noreferrer">
+                    <a href="https://github.com/MjolNear"
+                       className="text-gray-800 hover:text-blue-500"
+                       target="_blank"
+                       rel="noreferrer"
+                    >
                         <svg
                             aria-hidden="true"
                             focusable="false"

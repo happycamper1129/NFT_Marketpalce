a85190149564b37c3aec186e3778a3b3753e20f8
happycamper1129 <happycamper.sw@gmail.com>
2022-03-23 00:21:56 +0300

    Update BetaHeader.tsx

--- src/components/Layout/BetaHeader.tsx
+++ src/components/Layout/BetaHeader.tsx
@@ -4,8 +4,8 @@ const BetaHeader = () => {
     return (
         <div className="bg-blue-500 text-center py-0.5">
             <div className="inline-flex gap-1 font-archivo text-[14px] text-white">
-                Alpha version. Please report bugs
-                <a href="https://t.me/mj0lnear"
+                Beta version. Please report bugs
+                <a href="https://t.me/mj0lnearchat"
                    target="_blank"
                    rel="noreferrer"
                    className="underline font-semibold"
